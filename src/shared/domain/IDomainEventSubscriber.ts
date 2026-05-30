8bd365ca7a6ceeaf7f5ea7bc0aaaa6cef305d4dc
Crowslayer <crowslayer@gmail.com>
2026-05-30 15:13:49 -0600

    feat(IDomainEventSubscriber): add IDomainEventSubscriber interface for event subscription management
    Introduced the IDomainEventSubscriber interface, defining methods for subscribing to domain events and handling them. This addition enhances the event-driven architecture by providing a structured way to manage event subscriptions and improve decoupling within the domain.

---
+++ src/shared/domain/IDomainEventSubscriber.ts
@@ -0,0 +1,12 @@
+import { DomainEvent, DomainEventClass, SerializedDomainEvent } from '@shared/domain/DomainEvent';
+
+export type AnyDomainEvent = DomainEvent<string, unknown>;
+
+export type AnySerializedDomainEvent = SerializedDomainEvent<string, unknown>;
+
+export type AnyDomainEventClass = DomainEventClass<AnyDomainEvent, AnySerializedDomainEvent>;
+
+export interface IDomainEventSubscriber<TEvent extends AnyDomainEvent> {
+  subscribedTo(): Array<AnyDomainEventClass>;
+  on(event: TEvent): Promise<void>;
+}
