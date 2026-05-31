08d574ce056213337275777d0b69806cb8a14dd2
Crowslayer <crowslayer@gmail.com>
2026-05-31 12:04:35 -0600

    refactor(MockLogger): update logger methods to use vitest mocks
    Refactored the MockLogger class to utilize vitest for mocking logger methods. This change improves the consistency and reliability of the mock implementation, ensuring better integration with the testing framework.

--- tests/helpers/MockLogger.ts
+++ tests/helpers/MockLogger.ts
@@ -1,10 +1,11 @@
-import { ILogger } from '../../src/infrastructure/loggers/Logger'
+import { vi } from 'vitest';
+import { ILogger } from '../../src/infrastructure/loggers/Logger';
 
 export class MockLogger implements ILogger {
-  info = vi.fn()
-  warn = vi.fn()
-  error = vi.fn()
-  fatal = vi.fn()
-  debug = vi.fn()
-  trace = vi.fn()
+  info = vi.fn();
+  warn = vi.fn();
+  error = vi.fn();
+  fatal = vi.fn();
+  debug = vi.fn();
+  trace = vi.fn();
 }
