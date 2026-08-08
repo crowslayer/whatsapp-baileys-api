915374e382f61d366751a7a6f1a37e83832cc09b
Crowslayer <crowslayer@gmail.com>
2026-08-08 11:28:37 -0600

    refactor: enhance IWhatsAppRuntime interface with additional services
    Updated the IWhatsAppRuntime interface to include new properties for IPrivacyService and IChatStateService, improving the structure and accessibility of WhatsApp-related functionalities. This change enhances the overall interface design and aligns with the latest service integrations.

--- src/application/runtime/IWhatsAppRuntime.ts
+++ src/application/runtime/IWhatsAppRuntime.ts
@@ -1,6 +1,8 @@
+import { IChatStateService } from '@infrastructure/baileys/adapter/IChatStateService';
 import { IGroupService } from '@infrastructure/baileys/adapter/IGroupService';
 import { IMessageService } from '@infrastructure/baileys/adapter/IMessageService';
 import { IPresenceService } from '@infrastructure/baileys/adapter/IPresenceService';
+import { IPrivacyService } from '@infrastructure/baileys/adapter/IPrivacyService';
 import { IProfileService } from '@infrastructure/baileys/adapter/IProfileService';
 
 type DisconnectEvent = {
@@ -16,4 +18,6 @@ export interface IWhatsAppRuntime {
   get groups(): IGroupService;
   get profile(): IProfileService;
   get presence(): IPresenceService;
+  get privacy(): IPrivacyService;
+  get chatState(): IChatStateService;
 }
